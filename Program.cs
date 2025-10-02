7eb4d87adf75f43b1761696f8a309a9087ef721a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-10-02 20:35:36 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double cm, m, km;
+Console.WriteLine("Entre com a medida (em metros): ");
+string entrada = Console.ReadLine();
+m = Convert.ToDouble(entrada);
+cm = m * 100;
+km = m / 100;
+Console.WriteLine("Equivalência");
+
+
+Console.Write(cm);
+Console.WriteLine("cm");
+
+Console.Write(m);
+Console.WriteLine("m");
+
+
+Console.Write(km);
+Console.WriteLine("km");
